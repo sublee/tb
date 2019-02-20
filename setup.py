0a83d3d684870468eb0ffd8c5d85c901efd32a27
Hans Lee <hans.lee@kakaobrain.com>
2019-02-20 16:01:16 +0900

    tb -> teebee in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='tb',
+    name='teebee',
     version='0.0.1',
-    url='https://github.com/sublee/tb',
+    url='https://github.com/sublee/teebee',
     license='MIT',
     author='Heungsub Lee',
     author_email='sub@subl.ee',
     description='1k steps in TensorBoard for 1 epoch.',
     zip_safe=False,
     setup_requires=['tensorboardX'],
-    py_modules=['tb'],
+    py_modules=['teebee'],
 )
