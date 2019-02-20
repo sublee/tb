036423036eef0c467fdf70d4b0aefb94945a6c13
Hans Lee <hans.lee@kakaobrain.com>
2019-02-20 16:23:51 +0900

    Bump version: 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='teebee',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/sublee/teebee',
     license='MIT',
     author='Heungsub Lee',
