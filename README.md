12bca073fa6f73071836dc0d5c2bf53478bfa25f
Hans Lee <hans.lee@kakaobrain.com>
2019-02-20 16:01:26 +0900

    Remove magic with __getitem__

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # teebee
 
 ```python
-from teebee import TensorBoard
+from teebee import Teebee
 
-# Initialize a TensorBoard object with the per-epoch length
-# and the path where TensorBoard logs will be stored.
-tb = TensorBoard(len(loader), '/tmp/tb/run-20190219')
+# Initialize a Teebee object with the per-epoch length (same with the
+# length of the loader) and the path where TensorBoard logs will be stored.
+tb = Teebee(len(loader), '/tmp/tb/run-20190219')
 
 for epoch in range(1000):
-    # Write logs per epoch.
-    tb[epoch].scalar('lr', lr)
+    # Let tb follow the current epoch.
+    tb.epoch(epoch)
 
-    for i, batch in enumerate(loader):
+    # Report at the exact epoch.
+    tb.scalar('lr', lr)
+
+    for step, batch in enumerate(loader):
         ...
 
-        # Write logs per epoch/1000. Sometimes, writing would be skipped.
-        w = tb[epoch:i]
-        if w:
-            # Calculate data to log in this block.
-            # loss.item() is not cheap when using GPUs.
-            w.scalar('loss', loss.item())
+        # Let tb follow the current in-epoch step. It will return True
+        # if logs should be reported at this step.
+        if tb.step(step):
+            # Calculate data to report in this block.
+            tb.scalar('loss', loss.item())
 ```
--- teebee.py
+++ teebee.py
@@ -1,91 +1,124 @@
 """Makes 1 epoch fit to 1k steps in TensorBoard::
 
-    from teebee import TensorBoard
+    from teebee import Teebee
 
-    # Initialize a TensorBoard object with the per-epoch length
-    # and the path where TensorBoard logs will be stored.
-    tb = TensorBoard(len(loader), '/tmp/tb/run-20190219')
+    # Initialize a Teebee object with the per-epoch length (same with the
+    # length of the loader) and the path where TensorBoard logs will be stored.
+    tb = Teebee(len(loader), '/tmp/tb/run-20190219')
 
     for epoch in range(1000):
-        # Write logs per epoch.
-        tb[epoch].scalar('lr', lr)
+        # Let tb follow the current epoch.
+        tb.epoch(epoch)
 
-        for i, batch in enumerate(loader):
+        # Report at the exact epoch.
+        tb.scalar('lr', lr)
+
+        for step, batch in enumerate(loader):
             ...
 
-            # Write logs per epoch/1000. Sometimes, writing would be skipped.
-            w = tb[epoch:i]
-            if w:
-                # Calculate data to log in this block.
-                # loss.item() is not cheap when using GPUs.
-                w.scalar('loss', loss.item())
+            # Let tb follow the current in-epoch step. It will return True
+            # if logs should be reported at this step.
+            if tb.step(step):
+                # Calculate data to report in this block.
+                tb.scalar('loss', loss.item())
 
 """
-from typing import Optional, Union
+import logging
 
 from tensorboardX import SummaryWriter
 
-__all__ = ['TensorBoard']
-__version__ = '0.0.1'
+__all__ = ['Teebee']
+__version__ = '0.1.0'
 
 
-class Writer:
-    """A wrapper of :class:`SummaryWriter` to write logs at the specific step.
+logger = logging.getLogger(__name__)
+
+
+class Teebee:
+    """A TensorBoard writer that tracks training epochs and steps. It reports
+    1 epoch as 1k global steps in TensorBoard.
+
+    It disallows inverted steps. Always increase epoch and step.
+
+    Note:
+        The name of "Teebee" came from simply "TB" which is an acronym for
+        "TensorBoard".
+
     """
-
-    def __init__(self, writer: Optional[SummaryWriter], step: int):
-        self.writer = writer
-        self.step = step
-
-    def scalar(self, name: str, value: float):
-        """Writes a scalar log."""
-        if self.writer is None:
-            print('[%d] %s: %.5f' % (self.step, name, value))
-        else:
-            self.writer.add_scalar(name, value, self.step)
-
-
-class TensorBoard:
-    """..."""
+    __slots__ = ('epoch_length', 'writer', '_epoch', '_step', '_global_step')
 
     def __init__(self, epoch_length: int, path: str = None):
+        self.epoch_length = epoch_length
+
         if path is None:
             self.writer = None
         else:
             self.writer = SummaryWriter(path)
 
-        self.epoch_length = epoch_length
-        self.last_step = -1
+        self._epoch = -1
+        self._step = -1
+        self._global_step = -1
 
-    def __getitem__(self, epoch_i: Union[int, slice]) -> Optional[Writer]:
-        """Gets a :class:`Writer` at the given epoch::
+    def epoch(self, epoch: int):
+        """Sets the current epoch::
 
-            tb[epoch].scalar('x', x)  # [epoch] always returns a writer.
+            for epoch in range(epochs):
+                tb.epoch(epoch)
+                ...
 
-            w = tb[epoch:i]  # [epoch:i] can return None.
-            if w:
-                w.scalar('x', x)
+        Raises:
+            ValueError: If the given epoch is greater than or equals to the
+                previous epoch.
 
         """
-        try:
-            # [epoch:i]
-            epoch: int = epoch_i.start
-            i: int = epoch_i.stop
-        except AttributeError:
-            # [epoch]
-            epoch: int = epoch_i
-            i: int = None
+        if self._epoch >= epoch:
+            raise ValueError('already passed epoch: %d (old) >= %d (new)'
+                             '' % (self._epoch, self.epoch))
+        self._epoch = epoch
+        self._step = -1
 
-        # Calculate the global step based on the given epoch and i.
-        epoch_f = float(epoch)
-        if i is not None:
-            epoch_f += i / self.epoch_length
-        step = int(epoch_f * 1000)
+    def step(self, step: int) -> bool:
+        """Sets the current step in an epoch and returns whether it increases
+        the global step. Don't log anything if it returns ``False``::
 
-        if i is not None:
-            # When i given, skip already progressed step.
-            if self.last_step == step:
-                return None
-            self.last_step = step
+            for step, inputs in enumerate(loader):
+                ...
+                if tb.step(step):
+                    tb.scalar('loss', loss.item())
 
-        return Writer(self.writer, step)
+        Raises:
+            ValueError: If the given step is greater than or equals to the
+                previous step.
+
+        """
+        if self._step >= step:
+            raise ValueError('already passed step: %d (old) >= %d (new)'
+                             '' % (self._step, self.step))
+
+        prev_global_step = self.global_step
+        self._step = step
+        return self.global_step != prev_global_step
+
+    def global_step(self) -> int:
+        """Calculates the current global step. 1k of global step means 1
+        epoch.
+
+        Raises:
+            ValueError: If :meth:`epoch` has never been called.
+
+        """
+        if self._epoch < 0:
+            raise ValueError('epoch never set')
+
+        epoch_f = float(self._epoch)
+        if self._step >= 0:
+            epoch_f += self._step / self.epoch_length
+
+        return int(epoch_f * 1000)
+
+    def scalar(self, name: str, value: float):
+        """Writes a scalar log."""
+        if self.writer is None:
+            logger.debug('[%d] %s: %.5f', self.global_step, name, value)
+        else:
+            self.writer.add_scalar(name, value, self.global_step)
